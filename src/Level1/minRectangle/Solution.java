9308ea75ffa0122494d0380be4579372d9a7807e
Lucky-BE <byungeun96@gmail.com>
2022-10-10 22:54:33 +0900

    Level 1 - 최소직사각형

---
+++ src/Level1/minRectangle/Solution.java
@@ -0,0 +1,31 @@
+package Level1.minRectangle;
+
+public class Solution {
+    public static int solution(int[][] sizes) {
+        int maxW = 0, maxH = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            if(sizes[i][0] < sizes[i][1]){
+                int tmp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = tmp;
+            }
+            if(sizes[i][0] > maxW) {
+                maxW = sizes[i][0];
+            }
+            if(sizes[i][1] > maxH){
+                maxH = sizes[i][1];
+            }
+        }
+
+        return maxH * maxW;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[][] {{60, 50}, {30, 70}, {60, 30}, {80, 40}}));
+        System.out.println(solution(new int[][] {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}}));
+        System.out.println(solution(new int[][] {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}}));
+
+
+
+    }
+}
