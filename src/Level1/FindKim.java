74e43cbf4ef162003e6f951d639ede3ab5d46740
Lucky-BE <byungeun96@gmail.com>
2022-10-19 23:46:46 +0900

    level 1 - 서울에서 김서방 찾기

---
+++ src/Level1/FindKim.java
@@ -0,0 +1,19 @@
+package Level1;
+
+public class FindKim {
+    public static String solution(String[] seoul) {
+        String answer = "";
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                answer = "김서방은 "+ i +"에 있다";
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"Jane", "Kim"}));
+    }
+
+}
